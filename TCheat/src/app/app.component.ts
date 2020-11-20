169cfdcd93932accf7ca31a2612e5605c0c7284f
Vitali Pomanitski <vitali.pom@gmail.com>
2020-11-20 22:12:54 +0200

    created 3 columns of aleph-bet

--- TCheat/src/app/app.component.ts
+++ TCheat/src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'TCheat';
+  alephbet = [
+    'א',
+    'ב',
+    'ג',
+    'ד',
+    'ה',
+    'ו',
+    'ז',
+    'ח',
+    'ט',
+    'י',
+    'כ', 
+    'ל', 
+    'מ',
+    'נ',
+    'ס',
+    'ע',
+    'פ',
+    'צ',
+    'ק',
+    'ר',
+    'ש',
+    'ת',
+    'ם',
+    'ץ',
+    'ף',
+    'ך',
+  ];
 }
--- TCheat/src/app/app.module.ts
+++ TCheat/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    MatCardModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
