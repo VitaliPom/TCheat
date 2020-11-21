4d4d5fe01c1a56f03d1a77210c7368408b7c1a20
Vitali Pomanitski <vitali.pom@gmail.com>
2020-11-21 13:29:21 +0200

    Added padding to container + set width to be max-content

--- TCheat/src/app/app.module.ts
+++ TCheat/src/app/app.module.ts
@@ -7,6 +7,8 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +19,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatCardModule,
     MatButtonModule,
     MatButtonToggleModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
